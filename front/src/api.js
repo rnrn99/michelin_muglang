122fa2426b39be408251264d34ee41a8ef8fe45c
rnrn99 <giu0213@naver.com>
2022-04-21 15:59:46 +0900

    chore : port number 변경

--- front/src/api.js
+++ front/src/api.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 
-const backendPortNumber = "5001";
+const backendPortNumber = "5000";
 const serverUrl =
   "http://" + window.location.hostname + ":" + backendPortNumber + "/";
 
 async function get(endpoint, params = "") {
   console.log(
     `%cGET 요청 ${serverUrl + endpoint + "/" + params}`,
-    "color: #a25cd1;"
+    "color: #a25cd1;",
   );
 
   return axios.get(serverUrl + endpoint + "/" + params, {
