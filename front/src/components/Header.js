00749b5b44f77f3dd1b7a6de0dae16bfd735df38
rnrn99 <giu0213@naver.com>
2022-04-22 17:08:19 +0900

    chore : title 수정

--- front/src/components/Header.js
+++ front/src/components/Header.js
@@ -5,7 +5,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { logout } from "../redux/userSlice";
 
 import styles from "./css/header/Header.module.css";
-import { AppBar, Box, Toolbar, Typography, Link } from "@mui/material";
+import { AppBar, Toolbar, Typography, Link } from "@mui/material";
 
 function Header() {
   const dispatch = useDispatch();
@@ -23,87 +23,82 @@ function Header() {
   };
 
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar sx={{ bgcolor: "#ffbf69" }}>
-          <Link href="/" underline="none" sx={{ flexGrow: 1 }}>
-            <Typography
-              variant="h5"
-              component="div"
-              sx={{ fontWeight: "bold", color: "black" }}
+    <AppBar position="static" sx={{ flexGrow: 1 }}>
+      <Toolbar sx={{ bgcolor: "#ffbf69" }}>
+        <Typography
+          variant="h5"
+          component="div"
+          sx={{ fontWeight: "bold", color: "black", flexGrow: 1 }}
+        >
+          <a href="/" className={styles.title}>
+            미슐랭 먹을랭
+          </a>
+        </Typography>
+
+        <Link className={styles.menu__link}>
+          <span className={styles.menu__title}>
+            <span className={styles.menu__first_word} data-hover="미슐랭">
+              미슐랭
+            </span>
+            <span className={styles.menu__second_word} data-hover="찾아보기">
+              찾아보기
+            </span>
+          </span>
+        </Link>
+
+        <Link className={styles.menu__link}>
+          <span className={styles.menu__title}>
+            <span className={styles.menu__first_word} data-hover="팀">
+              팀
+            </span>
+            <span className={styles.menu__second_word} data-hover="소개">
+              소개
+            </span>
+          </span>
+        </Link>
+
+        {isLogin ? (
+          <>
+            <Link className={styles.menu__link}>
+              <span className={styles.menu__title}>
+                <span className={styles.menu__first_word} data-hover="마이">
+                  마이
+                </span>
+                <span className={styles.menu__second_word} data-hover="페이지">
+                  페이지
+                </span>
+              </span>
+            </Link>
+
+            <Link
+              className={styles.menu__link}
+              href="#"
+              onClick={logoutHandler}
             >
-              미슐랭 먹을랭
-            </Typography>
-          </Link>
-
-          <Link className={styles.menu__link}>
-            <span className={styles.menu__title}>
-              <span className={styles.menu__first_word} data-hover="미슐랭">
-                미슐랭
-              </span>
-              <span className={styles.menu__second_word} data-hover="찾아보기">
-                찾아보기
-              </span>
-            </span>
-          </Link>
-
-          <Link className={styles.menu__link}>
-            <span className={styles.menu__title}>
-              <span className={styles.menu__first_word} data-hover="팀">
-                팀
-              </span>
-              <span className={styles.menu__second_word} data-hover="소개">
-                소개
-              </span>
-            </span>
-          </Link>
-
-          {isLogin ? (
-            <>
-              <Link className={styles.menu__link}>
-                <span className={styles.menu__title}>
-                  <span className={styles.menu__first_word} data-hover="마이">
-                    마이
-                  </span>
-                  <span
-                    className={styles.menu__second_word}
-                    data-hover="페이지"
-                  >
-                    페이지
-                  </span>
-                </span>
-              </Link>
-
-              <Link
-                className={styles.menu__link}
-                href="#"
-                onClick={logoutHandler}
-              >
-                <span className={styles.menu__title}>
-                  <span className={styles.menu__first_word} data-hover="LOG">
-                    LOG
-                  </span>
-                  <span className={styles.menu__second_word} data-hover="OUT">
-                    OUT
-                  </span>
-                </span>
-              </Link>
-            </>
-          ) : (
-            <Link className={styles.menu__link} href="/login">
               <span className={styles.menu__title}>
                 <span className={styles.menu__first_word} data-hover="LOG">
                   LOG
                 </span>
-                <span className={styles.menu__second_word} data-hover="IN">
-                  IN
+                <span className={styles.menu__second_word} data-hover="OUT">
+                  OUT
                 </span>
               </span>
             </Link>
-          )}
-        </Toolbar>
-      </AppBar>
-    </Box>
+          </>
+        ) : (
+          <Link className={styles.menu__link} href="/login">
+            <span className={styles.menu__title}>
+              <span className={styles.menu__first_word} data-hover="LOG">
+                LOG
+              </span>
+              <span className={styles.menu__second_word} data-hover="IN">
+                IN
+              </span>
+            </span>
+          </Link>
+        )}
+      </Toolbar>
+    </AppBar>
   );
 }
 
