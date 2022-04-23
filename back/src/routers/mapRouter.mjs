2c4ddc383e2f79c53735f87ca9ed95dc9156a3d7
[4기-1반] 이동준 <0108dongjun@gmail.com>
2022-04-23 16:43:14 +0900

    feat : 백엔드 map 세계 마커 geojson으로 받는 기능 추가

--- back/src/routers/mapRouter.mjs
+++ back/src/routers/mapRouter.mjs
@@ -3,17 +3,17 @@ import { Router } from "express";
 
 const mapRouter = Router();
 
-//멀티 폴리곤으로 전세계 지도 주는데 프로퍼티에 국가 이름, 해당 국가의 미슐랭 음식점 수 있음
-mapRouter.get("/map", async (req, res, next) => {
+//geojson 마커로 프로퍼티에 국가 이름, 해당 국가의 미슐랭 음식점 수 있음
+mapRouter.get("/map/world/geojson", async (req, res, next) => {
   try {
-    const ret = await mapService.getWorldMap();
+    const ret = await mapService.getWorldMarker();
     res.status(200).json(ret);
   } catch (error) {
     next(error);
   }
 });
 
-//해당 국가의 모든 미슐랭 음식점 마커로 반환
+//해당 국가의 모든 미슐랭 음식점 geojson 마커로 반환
 mapRouter.get("/map/:country", async (req, res, next) => {
   try {
     const ret = await mapService.getCountryMarker(req.params.country);
--- back/src/services/mapService.mjs
+++ back/src/services/mapService.mjs
@@ -1,58 +1,29 @@
 import fs from "fs";
 import GeoJSON from "geojson";
 import { restaurantService } from "./restaurantService.mjs";
+import { Country } from "../db/index.mjs";
 
 class mapService {
-  //래스토랑 mvp 끝나면 db에서 가져오는걸로 바꿀 예정 지금은 일단 그냥 목데이터
-  static async getData() {
-    const jsonFile = await fs.promises.readFile("mockData.json");
-    const data = JSON.parse(jsonFile);
-    return data;
-  }
+  //세계지도 국가별 마커
+  static async getWorldMarker() {
+    let ret = await this.getCountryObj();
 
-  //세계지도 폴리곤
-  //기존 geojson에 쓸모 없는 프로퍼티가 너무 많아 name만 남기고 날리려고 코드 길게 썼는데 그냥 원본 geojson을 수정할 예정
-  static async getWorldMap() {
-    const jsonFile = await fs.promises.readFile("custom.geo.json");
-    const data = JSON.parse(jsonFile);
-    const countryObj = await this.getCountryObj();
-    data.features.forEach((e) => {
-      const name = e.properties.name;
-      const count = countryObj[name] || 0;
-      delete e.properties;
-      e.properties = { name, count };
-    });
+    //응답 받을떄 까지 2초 정도 걸리네요
+    //밑에 연산을 매번 할 필요는 없을것 같아서 그냥 db에다 count를 추가하는게 나을거 같기도 하네요
 
-    return data;
+    for (let i = 0; i < ret.length; i++) {
+      const c = await restaurantService.getRestaurantsByCountry({
+        restaurantCountry: ret[i].nation,
+      });
+      ret[i]["count"] = c.length;
+    }
+
+    return GeoJSON.parse(ret, { Point: ["lat", "lng"] });
   }
 
   //전체 국가 객체 반환
-  //이것도 db에 country 컬럼 추가되면 안쓸것 같음 일단 직접 슬라이싱 해서 country 개수 카운팅
   static async getCountryObj() {
-    let data = await this.getData();
-    const ret = {};
-    for (let i = 0; i < data.length; i++) {
-      const arr = data[i].address.split(", ");
-      const city = arr[arr.length - 1];
-      if (ret[city]) {
-        ret[city] += 1;
-      } else {
-        ret[city] = 1;
-      }
-    }
-    return ret;
-  }
-
-  //전체 마커 geojson으로 반환
-  //테스트용 그냥 전체 레스토랑 마커로 변환
-  static async getWorldMarker() {
-    let data = await this.getData();
-    let d = GeoJSON.parse(data, {
-      Point: ["latitude", "longitude"],
-      // include: ["name", "cuisine"],
-    });
-    console.log(d);
-    return d;
+    return Country.findByQuery({});
   }
 
   // 국가 마커 geojson으로 반환
@@ -60,7 +31,6 @@ class mapService {
     let ret = await restaurantService.getRestaurantsByCountry({
       restaurantCountry: country,
     });
-    console.log(ret[0]);
     return GeoJSON.parse(ret, { Point: ["latitude", "longitude"] });
   }
 }
