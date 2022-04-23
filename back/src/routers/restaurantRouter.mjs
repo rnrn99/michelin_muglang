6dfdde3394c121b43ed0d1d9a71ebd45586f0e67
eunna <dladmssk98@gmail.com>
2022-04-23 23:01:22 +0900

    chore : 주석 및 restaurant 에러 메시지 추가

--- back/src/routers/restaurantRouter.mjs
+++ back/src/routers/restaurantRouter.mjs
@@ -3,9 +3,9 @@ import { restaurantService } from "../services/restaurantService.mjs";
 
 const restaurantRouter = Router();
 
+// 전체 식당의 목록을 얻음
 restaurantRouter.get("/restaurants", async function (req, res, next) {
   try {
-    // 전체 식당 목록을 얻음
     const restaurants = await restaurantService.getRestaurants();
     res.status(200).send(restaurants);
   } catch (error) {
@@ -13,8 +13,10 @@ restaurantRouter.get("/restaurants", async function (req, res, next) {
   }
 });
 
+// 특정 식당의 정보를 얻음
 restaurantRouter.get("/restaurants/:id", async function (req, res, next) {
   try {
+    // URI로부터 restaurant_id를 추출함
     const restaurant_id = req.params.id;
     const restaurantInfo = await restaurantService.getRestaurantInfo({
       restaurant_id,
@@ -30,10 +32,12 @@ restaurantRouter.get("/restaurants/:id", async function (req, res, next) {
   }
 });
 
+// 특정 국가에 있는 식당들의 정보를 얻음
 restaurantRouter.get(
   "/restaurants/by/:country",
   async function (req, res, next) {
     try {
+      // URI로부터 국가 이름을 추출함
       const restaurantCountry = req.params.country;
       const restaurantsInfo = await restaurantService.getRestaurantsByCountry({
         restaurantCountry,
@@ -54,8 +58,11 @@ restaurantRouter.get(
   "/restaurants/:id/:currencyName",
   async function (req, res, next) {
     try {
+      // URI로부터 restaurant_id와 currency를 추출함
       const restaurant_id = req.params.id;
       const currencyName = req.params.currencyName;
+
+      // currency exchange된 minPrice와 maxPrice를 얻음
       const prices = await restaurantService.getConvertedPrice({
         restaurant_id,
         currencyName,
--- back/src/services/restaurantService.mjs
+++ back/src/services/restaurantService.mjs
@@ -9,7 +9,7 @@ class restaurantService {
   static async getRestaurantInfo({ restaurant_id }) {
     const restaurant = await Restaurant.findById({ restaurant_id });
 
-    // db에서 찾지 못한 경우, 에러 메시지 반환
+    // db에서 해당 식당을 찾지 못한 경우, 에러 메시지 반환
     if (!restaurant) {
       const errorMessage = "해당 레스토랑은 존재하지 않습니다.";
       return { errorMessage };
@@ -23,7 +23,7 @@ class restaurantService {
       country: restaurantCountry,
     });
 
-    // db에서 찾지 못한 경우, 에러 메시지 반환
+    // db에서 해당 국가에 존재하는 식당을 찾지 못한 경우, 에러 메시지 반환
     if (!restaurants) {
       const errorMessage = "해당 국가에 식당이 존재하지 않습니다.";
       return { errorMessage };
@@ -34,6 +34,14 @@ class restaurantService {
 
   static async getConvertedPrice({ restaurant_id, currencyName }) {
     const restaurant = await Restaurant.findById({ restaurant_id });
+
+    // db에서 해당 식당을 찾지 못한 경우, 에러 메시지 반환
+    if (!restaurant) {
+      const errorMessage = "해당 레스토랑은 존재하지 않습니다.";
+      return { errorMessage };
+    }
+
+    // 현재 식당의 최고가, 최저가, 통화를 확인
     const [minPrice, maxPrice, currentCurrencyName] = [
       restaurant.minPrice,
       restaurant.maxPrice,
@@ -57,19 +65,20 @@ class restaurantService {
       return { errorMessage };
     }
 
+    // 현재 통화와 타켓 통화가 일치할 경우 기존 값을 반환
     if (currentCurrency.currency == targetCurrency.currency) {
       return { minPrice, maxPrice };
     }
 
     let convertedMinPrice, convertedMaxPrice;
+
+    // exchange_rates가 EUR를 기준으로 value값이 설정 되어 있으므로 타겟 통화의 value로 계산
     if (currentCurrency == "EUR") {
       convertedMinPrice = minPrice * targetCurrency.value;
       convertedMaxPrice = maxPrice * targetCurrency.value;
     } else {
-      console.log("find!");
-      console.log(currentCurrency.value, targetCurrency.value);
+      // EUR를 기준으로 비율 계산 (타겟 통화 value / 현재 통화 value)
       const ratio = targetCurrency.value / currentCurrency.value;
-      console.log(ratio);
       convertedMinPrice = minPrice * ratio;
       convertedMaxPrice = maxPrice * ratio;
     }
