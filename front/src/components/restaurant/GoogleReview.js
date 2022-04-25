40672e156ac12f72148d44b26d0d5bec620c1515
seon <seonsy44@gmail.com>
2022-04-25 15:30:44 +0900

    feat: 구글 리뷰 별점 구현

--- front/src/components/restaurant/GoogleReview.js
+++ front/src/components/restaurant/GoogleReview.js
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
 import styles from "../../css/restaurant/GoogleReview.module.css";
+import StarIcon from "@mui/icons-material/Star";
 
 const GoogleReview = ({ review }) => {
   const [detail, setDetail] = useState(false);
+  const starYellowWidth = review.rating * 15.4;
 
   const reviewText = detail ? (
     review.text
@@ -27,11 +29,19 @@ const GoogleReview = ({ review }) => {
   return (
     <div className={styles.review} key={review.time}>
       <div className={styles.review_title}>
-        <img src={review.profile_photo_url} />
+        <img src={review.profile_photo_url} alt="×" />
         <span>{review.author_name}</span>
       </div>
       <div className={styles.review_rating}>
-        <div>별 {review.rating}개</div>
+        <div className={styles.star}>
+          <div className={styles.star_gray}>★ ★ ★ ★ ★</div>
+          <div
+            className={styles.star_yellow}
+            style={{ width: starYellowWidth }}
+          >
+            ★ ★ ★ ★ ★
+          </div>
+        </div>
         <span>{review.relative_time_description}</span>
       </div>
       <div className={styles.review_content}>{reviewText}</div>
--- front/src/components/restaurant/GoogleReviews.js
+++ front/src/components/restaurant/GoogleReviews.js
@@ -1,24 +1,39 @@
-import React, { useEffect } from "react";
+import React from "react";
 import GoogleReview from "./GoogleReview";
 import styles from "../../css/restaurant/GoogleReviews.module.css";
 import data from "./googleReviewMock.json";
+import StarIcon from "@mui/icons-material/Star";
 
 const GoogleReviews = () => {
   const result = JSON.parse(JSON.stringify(data));
   const totalRating = result.rating;
+  const starYellowWidth = totalRating * 24;
   const reviews = result.reviews;
-
-  const ratings = { 1: 0, 2: 0, 3: 0, 4: 0, 5: 0 };
-  useEffect(() => {
-    reviews.map((review) => (ratings[review.rating] += 1));
-  }, []);
 
   return (
     <div className={styles.container}>
       <span className={styles.title}>Google Review</span>
       <div className={styles.rating}>
-        <div>{totalRating}</div>
-        <div>리뷰 {reviews.length}개</div>
+        <div className={styles.rating_number}>{totalRating}</div>
+        <div className={styles.star}>
+          <div className={styles.star_gray}>
+            <StarIcon />
+            <StarIcon />
+            <StarIcon />
+            <StarIcon />
+            <StarIcon />
+          </div>
+          <div
+            className={styles.star_yellow}
+            style={{ width: starYellowWidth }}
+          >
+            <StarIcon />
+            <StarIcon />
+            <StarIcon />
+            <StarIcon />
+            <StarIcon />
+          </div>
+        </div>
       </div>
       <div className={styles.reviews}>
         {reviews.map((review) => {
