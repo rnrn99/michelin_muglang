61ece3de664ddbf02a4a3747ec5fe20b6e9d3aae
rnrn99 <giu0213@naver.com>
2022-04-26 16:18:48 +0900

    feat : pie chart 변경

--- front/src/components/main/SectionCovid.js
+++ front/src/components/main/SectionCovid.js
@@ -1,66 +1,35 @@
 import React, { useState, useEffect } from "react";
 import * as Api from "../../api";
-import {
-  LineChart,
-  Line,
-  XAxis,
-  YAxis,
-  CartesianGrid,
-  Tooltip,
-  Legend,
-} from "recharts";
+import { PieChart, Pie, Legend, Tooltip, Cell } from "recharts";
 
 function SectionCovid() {
-  const [graph, setGraph] = useState([
-    {
-      _id: "6263b52e2e6be8c14a23cf24",
-      type: "covidMonthly",
-      year: "2022-01",
-      case: 107275.0935483871,
-    },
-    {
-      _id: "6263b52e2e6be8c14a23cf25",
-      type: "covidMonthly",
-      year: "2022-02",
-      case: 71885.61785714286,
-    },
-    {
-      _id: "6263b52e2e6be8c14a23cf26",
-      type: "covidMonthly",
-      year: "2022-03",
-      case: 60323.461764705884,
-    },
-  ]); // graph 그릴 data를 저장할 상태
+  const [graph, setGraph] = useState([]); // graph 그릴 data를 저장할 상태
+  const COLORS = ["#1300ff", "#ff5e5e"];
 
   useEffect(() => {
-    Api.get("graph/vaccinatedRatio").then((res) => console.log(res.data));
+    Api.get("graphs/covid-weekly").then((res) => setGraph(res.data));
   }, []);
+
   return (
-    <div style={{ border: "2px solid blue" }}>
-      Covid
-      <LineChart
-        width={500}
-        height={300}
-        data={graph}
-        margin={{
-          top: 5,
-          right: 30,
-          left: 20,
-          bottom: 5,
-        }}
-      >
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="year" />
-        <YAxis dataKey="case" />
-        <Tooltip />
+    <div style={{ border: "1px solid blue" }}>
+      <PieChart width={400} height={400}>
+        <Pie
+          dataKey="percent"
+          isAnimationActive={false}
+          data={graph}
+          cx="50%"
+          cy="50%"
+          innerRadius={70}
+          outerRadius={150}
+          label
+        >
+          {graph.map((e, i) => (
+            <Cell key={`cell-${i}`} fill={COLORS[i % COLORS.length]} />
+          ))}
+        </Pie>
         <Legend />
-        <Line
-          type="monotone"
-          dataKey="case"
-          stroke="#8884d8"
-          activeDot={{ r: 8 }}
-        />
-      </LineChart>
+        <Tooltip />
+      </PieChart>
     </div>
   );
 }
