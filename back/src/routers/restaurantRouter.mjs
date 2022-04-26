5efacdf04368f79d8814319d2ccad6dd6a1389ec
eunna <dladmssk98@gmail.com>
2022-04-26 17:39:03 +0900

    fix : restaurant Router를 RESTful하게 수정

--- back/src/routers/restaurantRouter.mjs
+++ back/src/routers/restaurantRouter.mjs
@@ -3,8 +3,29 @@ import { restaurantService } from "../services/restaurantService.mjs";
 
 const restaurantRouter = Router();
 
-// 전체 식당의 목록을 얻음
+// Path: /restaurants
 restaurantRouter.get("/restaurants", async function (req, res, next) {
+  // 특정 국가에 있는 식당들의 정보를 얻음
+  if (req.query.country) {
+    try {
+      // URI로부터 country(query)를 추출함
+      const country = req.query.country;
+      const restaurants = await restaurantService.getRestaurantsByCountry({
+        country,
+      });
+
+      if (restaurants.errorMessage) {
+        throw new Error(restaurants.errorMessage);
+      }
+
+      res.status(200).send(restaurants);
+      return;
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  // 전체 식당의 목록을 얻음
   try {
     const restaurants = await restaurantService.getRestaurants();
     res.status(200).send(restaurants);
@@ -13,59 +34,19 @@ restaurantRouter.get("/restaurants", async function (req, res, next) {
   }
 });
 
-// 특정 식당의 정보를 얻음
+// Path: /restaurants/:id
 restaurantRouter.get("/restaurants/:id", async function (req, res, next) {
-  try {
-    // URI로부터 restaurant_id를 추출함
-    const restaurant_id = req.params.id;
-    const restaurantInfo = await restaurantService.getRestaurantInfo({
-      restaurant_id,
-    });
-
-    if (restaurantInfo.errorMessage) {
-      throw new Error(restaurantInfo.errorMessage);
-    }
-
-    res.status(200).send(restaurantInfo);
-  } catch (error) {
-    next(error);
-  }
-});
-
-// 특정 국가에 있는 식당들의 정보를 얻음
-restaurantRouter.get(
-  "/restaurants/by/:country",
-  async function (req, res, next) {
+  // 특정 식당의 가격에 대한 환전 결과를 얻음
+  if (req.query.currency) {
     try {
-      // URI로부터 국가 이름을 추출함
-      const restaurantCountry = req.params.country;
-      const restaurantsInfo = await restaurantService.getRestaurantsByCountry({
-        restaurantCountry,
-      });
-
-      if (restaurantsInfo.errorMessage) {
-        throw new Error(restaurantsInfo.errorMessage);
-      }
-
-      res.status(200).send(restaurantsInfo);
-    } catch (error) {
-      next(error);
-    }
-  },
-);
-
-restaurantRouter.get(
-  "/restaurants/:id/:currencyName",
-  async function (req, res, next) {
-    try {
-      // URI로부터 restaurant_id와 currency를 추출함
-      const restaurant_id = req.params.id;
-      const currencyName = req.params.currencyName;
+      // URI로부터 restaurant_id(params)와 currency(query)를 추출함
+      const id = req.params.id;
+      const currencyCode = req.query.currency;
 
       // currency exchange된 minPrice와 maxPrice를 얻음
       const prices = await restaurantService.getConvertedPrice({
-        restaurant_id,
-        currencyName,
+        id,
+        currencyCode,
       });
 
       if (prices.errorMessage) {
@@ -73,10 +54,26 @@ restaurantRouter.get(
       }
 
       res.status(200).send(prices);
+      return;
     } catch (error) {
       next(error);
     }
-  },
-);
+  }
+
+  // 특정 식당의 정보를 얻음
+  try {
+    // URI로부터 restaurant_id를 추출함
+    const id = req.params.id;
+    const restaurant = await restaurantService.getRestaurantInfo({ id });
+
+    if (restaurant.errorMessage) {
+      throw new Error(restaurant.errorMessage);
+    }
+
+    res.status(200).send(restaurant);
+  } catch (error) {
+    next(error);
+  }
+});
 
 export { restaurantRouter };
