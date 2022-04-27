c35dcc798a7d4e03f9695331805445bda8c9eea5
seon <seonsy44@gmail.com>
2022-04-27 14:14:11 +0900

    feat : 북마크 카드 구현

--- front/src/components/user/mypage/MyBookmark.js
+++ front/src/components/user/mypage/MyBookmark.js
@@ -1,12 +1,38 @@
 import React from "react";
 import Slider from "./Slider";
 import styles from "../../../css/user/MyBookmark.module.css";
+import DeleteIcon from "@mui/icons-material/Delete";
 const mock = [
-  { name: "The Table Kevin Fehling", country: "Germany" },
-  { name: "Aqua", country: "Germany" },
-  { name: "Restaurant Überfahrt Christian Jürgens", country: "Germany" },
-  { name: "Restaurant Überfahrt Christian Jürgens", country: "Germany" },
-  { name: "Restaurant Überfahrt Christian Jürgens", country: "Germany" },
+  {
+    _id: "626370999dcb2f0f3041e88a",
+    name: "Aqua",
+    country: "Germany",
+    bookmarkCount: 1,
+  },
+  {
+    _id: "626370999dcb2f0f3041e88b",
+    name: "No. 69 Fangzhuanchang Zhajiangmian (Fangzhuanchang Hutong)",
+    country: "China",
+    bookmarkCount: 1,
+  },
+  {
+    _id: "626370999dcb2f0f3041e88c",
+    name: "L'Auberge de St-Rémy-de-Provence - Fanny Rey & Jonathan Wahid",
+    country: "France",
+    bookmarkCount: 1,
+  },
+  {
+    _id: "626370999dcb2f0f3041e88d",
+    name: "The Table Kevin Fehling",
+    country: "Germany",
+    bookmarkCount: 1,
+  },
+  {
+    _id: "626370999dcb2f0f3041e88e",
+    name: "Fried Banana Rama 5",
+    country: "Thailand",
+    bookmarkCount: 1,
+  },
 ];
 
 const MyBookmark = () => {
@@ -15,7 +41,15 @@ const MyBookmark = () => {
       <div className={styles.title}>나의 북마크</div>
       <Slider contentLength={mock.length}>
         {mock.map((restaurant) => (
-          <div className={styles.bookmark}>{restaurant.name}</div>
+          <div className={styles.restaurant}>
+            <span className={styles.restaurant_name}>{restaurant.name}</span>
+            <span className={styles.restaurant_country}>
+              {restaurant.country}
+            </span>
+            <div className={styles.icon_delete}>
+              <DeleteIcon />
+            </div>
+          </div>
         ))}
       </Slider>
     </div>
