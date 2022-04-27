1580bec1e72dcd5743f1c777dc249b65bbadd619
eunna <dladmssk98@gmail.com>
2022-04-27 16:49:24 +0900

    fix : error 처리 추가 및 세분화

--- back/src/services/restaurantService.mjs
+++ back/src/services/restaurantService.mjs
@@ -8,6 +8,14 @@ class restaurantService {
 
   static async getRestaurantsPaging({ page, pageSize }) {
     const restaurants = await Restaurant.findAllPaging({ page, pageSize });
+
+    // 페이징 과정 중 실패한 경우, 에러 메시지 반환
+    if (!restaurants) {
+      const error = new Error("레스토랑을 페이징하지 못하였습니다.");
+      error.statusCode = 500;
+      throw error;
+    }
+
     return restaurants;
   }
 
@@ -16,8 +24,9 @@ class restaurantService {
 
     // db에서 해당 식당을 찾지 못한 경우, 에러 메시지 반환
     if (!restaurant) {
-      const errorMessage = "해당 레스토랑은 존재하지 않습니다.";
-      return { errorMessage };
+      const error = new Error("해당 레스토랑은 존재하지 않습니다.");
+      error.statusCode = 400;
+      throw error;
     }
 
     return restaurant;
@@ -28,8 +37,9 @@ class restaurantService {
 
     // db에서 해당 국가에 존재하는 식당을 찾지 못한 경우, 에러 메시지 반환
     if (!restaurants) {
-      const errorMessage = "해당 국가에 식당이 존재하지 않습니다.";
-      return { errorMessage };
+      const error = new Error("해당 국가에 식당이 존재하지 않습니다.");
+      error.statusCode = 400;
+      throw error;
     }
 
     return restaurants;
@@ -44,8 +54,9 @@ class restaurantService {
 
     // db에서 해당 국가에 존재하는 식당을 찾지 못한 경우, 에러 메시지 반환
     if (!restaurants) {
-      const errorMessage = "해당 국가에 식당이 존재하지 않습니다.";
-      return { errorMessage };
+      const error = new Error("해당 국가에 식당이 존재하지 않습니다.");
+      error.statusCode = 400;
+      throw error;
     }
 
     return restaurants;
@@ -60,8 +71,11 @@ class restaurantService {
 
     // db에서 해당 국가에 존재하는 식당을 찾지 못한 경우, 에러 메시지 반환
     if (!restaurants) {
-      const errorMessage = "해당 국가에 식당이 존재하지 않습니다.";
-      return { errorMessage };
+      const error = new Error(
+        "해당 음식 카테고리로 분류되는 식당이 존재하지 않습니다.",
+      );
+      error.statusCode = 400;
+      throw error;
     }
 
     return restaurants;
@@ -72,16 +86,19 @@ class restaurantService {
 
     // db에서 해당 식당을 찾지 못한 경우, 에러 메시지 반환
     if (!restaurant) {
-      const errorMessage = "해당 레스토랑은 존재하지 않습니다.";
-      return { errorMessage };
+      const error = new Error("해당 레스토랑은 존재하지 않습니다.");
+      error.statusCode = 400;
+      throw error;
     }
 
     // 현재 식당의 최고가, 최저가, 통화를 확인
     const { minPrice, maxPrice, currency: currentCurrencyCode } = restaurant;
+
     // 가격 정보를 제공하지 않는 식당의 경우, 에러 메시지 반환
     if (!minPrice) {
-      const errorMessage = "해당 레스토랑은 가격 정보를 제공하지 않습니다.";
-      return { errorMessage };
+      const error = new Error("해당 레스토랑은 가격 정보를 제공하지 않습니다.");
+      error.statusCode = 400;
+      throw error;
     }
 
     const targetCurrency = await Currency.findByCode({ code: currencyCode });
@@ -91,9 +108,15 @@ class restaurantService {
 
     // db에서 해당 통화를 찾지 못한 경우, 에러 메시지 반환
     if (!targetCurrency) {
-      console.log(targetCurrency, currencyCode);
-      const errorMessage = "해당 통화에 대한 정보가 존재하지 않습니다.";
-      return { errorMessage };
+      const error = new Error("해당 통화에 대한 정보가 존재하지 않습니다.");
+      error.statusCode = 400;
+      throw error;
+    }
+
+    if (!currentCurrency) {
+      const error = new Error("통화에 대한 정보를 가져오는 데 실패하였습니다.");
+      error.statusCode = 500;
+      throw error;
     }
 
     // 현재 통화와 타켓 통화가 일치할 경우 기존 값을 반환
