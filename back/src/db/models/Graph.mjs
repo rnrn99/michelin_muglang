5a0dce3ae4c2553a142bb2e3446fe9fd4500b734
12yoonj <12yoonj@gmail.com>
2022-04-28 12:33:26 +0900

    docs: 주석 삭제

--- back/src/db/models/Graph.mjs
+++ back/src/db/models/Graph.mjs
@@ -3,7 +3,6 @@ import { db } from "../index.mjs";
 class Graph {
   static findItems = async (type) => {
     const items = await db.collection("graphItems").find({ type }).toArray();
-    // .forEach((element) => items.push(element));
     return items;
   };
 }
