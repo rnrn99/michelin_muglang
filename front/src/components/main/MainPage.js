ae692cbe1e478d0926d6faef691f669aeeb99f47
rnrn99 <giu0213@naver.com>
2022-04-29 15:37:21 +0900

    feat : section 0, 3 반응형 적용

--- front/src/components/main/MainPage.js
+++ front/src/components/main/MainPage.js
@@ -72,15 +72,17 @@ function MainPage() {
             <span>끝나가는 코로나!</span>{" "}
             <span>이제는 맛집을 찾으러 여행할 타이밍.</span>
           </h1>
-          <p>미슐랭 먹을랭과 함께 전세계 맛집을 탐방해요!</p>
+          <div>
+            <p>미슐랭 먹을랭과 함께 전세계 맛집을 탐방해요!</p>
 
-          <a href="/">
-            <span className={styles.text}>미슐랭 찾아보기 →</span>
-            <span className={`${styles.line} ${styles.right}`}></span>
-            <span className={`${styles.line} ${styles.top}`}></span>
-            <span className={`${styles.line} ${styles.left}`}></span>
-            <span className={`${styles.line} ${styles.bottom}`}></span>
-          </a>
+            <a href="/">
+              <span className={styles.text}>미슐랭 찾아보기 →</span>
+              <span className={`${styles.line} ${styles.right}`}></span>
+              <span className={`${styles.line} ${styles.top}`}></span>
+              <span className={`${styles.line} ${styles.left}`}></span>
+              <span className={`${styles.line} ${styles.bottom}`}></span>
+            </a>
+          </div>
         </div>
         <div className={styles.greeting_img}>
           <img src="images/greeting.png" alt="main_img" />
