8788fb5659b49774d7117c156061cd2ca6673c18
seon <seonsy44@gmail.com>
2022-04-29 17:20:18 +0900

    chore : 안쓰는 코드 삭제

--- front/src/components/user/mypage/MyBookmark.js
+++ front/src/components/user/mypage/MyBookmark.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import { deleteBookmark } from "../../../redux/userSlice";
 import Slider from "./Slider";
