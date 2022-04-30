24b218acfe42a3e74b621fb81fc3b2b2b814fed1
bsy1141 <bsy1141@naver.com>
2022-04-30 16:35:33 +0900

    fix: 경로 수정

--- front/src/components/map/RestaurantCard.js
+++ front/src/components/map/RestaurantCard.js
@@ -34,7 +34,7 @@ const RestaurantCard = ({ restaurant, handleClick, clicked, goToList }) => {
         </span>
         {clicked && (
           <div className={styles.detailPageBtn}>
-            <a>상세 정보 바로가기</a>
+            <a href={`/restaurants/${restaurant._id}`}>상세 정보 바로가기</a>
           </div>
         )}
       </div>
