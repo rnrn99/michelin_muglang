6f3ff77062a6e44bf902b42807296b63fa7c2377
seon <seonsy44@gmail.com>
2022-05-03 15:18:19 +0900

    chore : placeId 없는 레스토랑은 address로 처리

--- front/src/components/restaurant/Googlemap.js
+++ front/src/components/restaurant/Googlemap.js
@@ -1,28 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 
 const Googlemap = () => {
   const GOOGLE_API_KEY = process.env.REACT_APP_GOOGLE_API_KEY;
-  const { placeId } = useSelector((state) => state.restaurant.restaurantInfo);
+  const { placeId, address } = useSelector(
+    (state) => state.restaurant.restaurantInfo,
+  );
+  const [src, setSrc] = useState("");
+
+  useEffect(() => {
+    if (placeId !== "aaa") {
+      setSrc(
+        "https://www.google.com/maps/embed/v1/place?key=" +
+          GOOGLE_API_KEY +
+          "&q=place_id:" +
+          placeId,
+      );
+    } else {
+      setSrc(
+        "https://www.google.com/maps/embed/v1/place?key=" +
+          GOOGLE_API_KEY +
+          "&q=" +
+          address,
+      );
+    }
+  }, [placeId]);
 
   return (
-    <>
-      {placeId && (
-        <iframe
-          width="540"
-          height="380"
-          frameBorder="0"
-          referrerPolicy="no-referrer-when-downgrade"
-          src={
-            "https://www.google.com/maps/embed/v1/place?key=" +
-            GOOGLE_API_KEY +
-            "&q=place_id:" +
-            placeId
-          }
-          allowFullScreen
-        ></iframe>
-      )}
-    </>
+    <iframe
+      width="540"
+      height="380"
+      frameBorder="0"
+      referrerPolicy="no-referrer-when-downgrade"
+      src={src}
+      allowFullScreen
+    ></iframe>
   );
 };
 
