a1a8ff4061bcd8c6abcf575d0a9bc36ac2549074
seon <seonsy44@gmail.com>
2022-05-03 17:56:04 +0900

    chore : placeId 없는 경우 구글 리뷰 안보이게 처리

--- front/src/components/restaurant/GoogleReviews.js
+++ front/src/components/restaurant/GoogleReviews.js
@@ -12,7 +12,7 @@ const GoogleReviews = () => {
   const starYellowWidth = rating * 24;
 
   useEffect(() => {
-    if (placeId !== "aaa") {
+    if (placeId) {
       get("google", placeId).then((res) => {
         setReviews(res.data.result.reviews);
         setRating(res.data.result.rating);
@@ -21,36 +21,40 @@ const GoogleReviews = () => {
   }, [placeId]);
 
   return (
-    <div className={styles.container}>
-      <span className={styles.title}>Google Review</span>
-      <div className={styles.rating}>
-        <div className={styles.rating_number}>{rating}</div>
-        <div className={styles.star}>
-          <div className={styles.star_gray}>
-            <StarIcon />
-            <StarIcon />
-            <StarIcon />
-            <StarIcon />
-            <StarIcon />
+    <>
+      {reviews.length > 0 && (
+        <div className={styles.container}>
+          <span className={styles.title}>Google Review</span>
+          <div className={styles.rating}>
+            <div className={styles.rating_number}>{rating}</div>
+            <div className={styles.star}>
+              <div className={styles.star_gray}>
+                <StarIcon />
+                <StarIcon />
+                <StarIcon />
+                <StarIcon />
+                <StarIcon />
+              </div>
+              <div
+                className={styles.star_yellow}
+                style={{ width: starYellowWidth }}
+              >
+                <StarIcon />
+                <StarIcon />
+                <StarIcon />
+                <StarIcon />
+                <StarIcon />
+              </div>
+            </div>
           </div>
-          <div
-            className={styles.star_yellow}
-            style={{ width: starYellowWidth }}
-          >
-            <StarIcon />
-            <StarIcon />
-            <StarIcon />
-            <StarIcon />
-            <StarIcon />
+          <div className={styles.reviews}>
+            {reviews.map((review) => {
+              return <GoogleReview review={review} key={review.time} />;
+            })}
           </div>
         </div>
-      </div>
-      <div className={styles.reviews}>
-        {reviews.map((review) => {
-          return <GoogleReview review={review} key={review.time} />;
-        })}
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
