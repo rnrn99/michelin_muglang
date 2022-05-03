0fbd52a5689aade1fa736906c81327fc4e9912bb
rnrn99 <giu0213@naver.com>
2022-05-03 13:08:27 +0900

    feat : info page 구조 잡기

---
+++ front/src/components/info/MichelinGraph.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function MichelinGraph() {
+  return <div>MichelinGraph</div>;
+}
+
+export default MichelinGraph;
---
+++ front/src/components/info/MonthlyGraph.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function MonthlyGraph() {
+  return <div>MonthlyGraph</div>;
+}
+
+export default MonthlyGraph;
