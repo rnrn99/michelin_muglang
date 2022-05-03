0c574004f5bfe0bdbbd3e29dfe6c2bd465e94897
seon <seonsy44@gmail.com>
2022-05-03 17:52:07 +0900

    chore : 구글 리뷰 백엔드에 연결

--- front/src/components/restaurant/GoogleReviews.js
+++ front/src/components/restaurant/GoogleReviews.js
@@ -1,39 +1,30 @@
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
+import { get } from "../../api";
 import GoogleReview from "./GoogleReview";
 import styles from "../../css/restaurant/GoogleReviews.module.css";
-import data from "./googleReviewMock.json";
 import { Star as StarIcon } from "@mui/icons-material";
-import axios from "axios";
 
 const GoogleReviews = () => {
-  const GOOGLE_API_KEY = process.env.REACT_APP_GOOGLE_API_KEY;
   const { placeId } = useSelector((state) => state.restaurant.restaurantInfo);
-  const result = JSON.parse(JSON.stringify(data));
-  const reviews = result.reviews;
-  // const [reviews, setReviews] = useState([]);
-  // const [rating, setRating] = useState(0);
-  const starYellowWidth = result.rating * 24;
+  const [reviews, setReviews] = useState([]);
+  const [rating, setRating] = useState(0);
+  const starYellowWidth = rating * 24;
 
-  // useEffect(() => {
-  //   let config = {
-  //     method: "get",
-  //     url: `https://maps.googleapis.com/maps/api/place/details/json?place_id=${placeId}&key=${GOOGLE_API_KEY}`,
-  //     headers: {},
-  //   };
-
-  //   if (placeId) {
-  //     axios(config).then((res) => {
-  //       console.log(JSON.stringify(res.data));
-  //     });
-  //   }
-  // }, [placeId]);
+  useEffect(() => {
+    if (placeId !== "aaa") {
+      get("google", placeId).then((res) => {
+        setReviews(res.data.result.reviews);
+        setRating(res.data.result.rating);
+      });
+    }
+  }, [placeId]);
 
   return (
     <div className={styles.container}>
       <span className={styles.title}>Google Review</span>
       <div className={styles.rating}>
-        <div className={styles.rating_number}>{result.rating}</div>
+        <div className={styles.rating_number}>{rating}</div>
         <div className={styles.star}>
           <div className={styles.star_gray}>
             <StarIcon />
