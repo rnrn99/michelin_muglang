f83eb586b2e52cad4156ca8b4574d339dc8b13d5
seon <seonsy44@gmail.com>
2022-05-03 11:34:26 +0900

    chore : 이미지 경로 변경

--- front/src/components/user/UnregisterConfirmationPage.js
+++ front/src/components/user/UnregisterConfirmationPage.js
@@ -7,7 +7,10 @@ const UnregisterConfirmationPage = () => {
 
   return (
     <div className={styles.container}>
-      <img src="unregister.svg" alt="회원탈퇴" />
+      <img
+        src={`${process.env.PUBLIC_URL}/images/unregister.svg`}
+        alt="회원탈퇴"
+      />
       <div>회원탈퇴가 완료되었습니다.</div>
       <div>그동한 미슐랭 먹을랭 서비스를 이용해 주셔서 감사합니다.</div>
       <button
