9024f780c929cddc1993fa3296d25d9d77a7f460
seon <seonsy44@gmail.com>
2022-05-03 14:57:30 +0900

    chore : cors에러부분 잠시 주석처리

--- front/src/components/restaurant/GoogleReviews.js
+++ front/src/components/restaurant/GoogleReviews.js
@@ -1,20 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 import GoogleReview from "./GoogleReview";
 import styles from "../../css/restaurant/GoogleReviews.module.css";
 import data from "./googleReviewMock.json";
 import { Star as StarIcon } from "@mui/icons-material";
+import axios from "axios";
 
 const GoogleReviews = () => {
+  const GOOGLE_API_KEY = process.env.REACT_APP_GOOGLE_API_KEY;
+  const { placeId } = useSelector((state) => state.restaurant.restaurantInfo);
   const result = JSON.parse(JSON.stringify(data));
-  const totalRating = result.rating;
-  const starYellowWidth = totalRating * 24;
   const reviews = result.reviews;
+  // const [reviews, setReviews] = useState([]);
+  // const [rating, setRating] = useState(0);
+  const starYellowWidth = result.rating * 24;
+
+  // useEffect(() => {
+  //   let config = {
+  //     method: "get",
+  //     url: `/maps/api/place/details/json?place_id=${placeId}&key=${GOOGLE_API_KEY}`,
+  //     headers: {},
+  //   };
+
+  //   if (placeId) {
+  //     axios(config).then((res) => {
+  //       console.log(JSON.stringify(res.data));
+  //     });
+  //   }
+  // }, [placeId]);
 
   return (
     <div className={styles.container}>
       <span className={styles.title}>Google Review</span>
       <div className={styles.rating}>
-        <div className={styles.rating_number}>{totalRating}</div>
+        <div className={styles.rating_number}>{result.rating}</div>
         <div className={styles.star}>
           <div className={styles.star_gray}>
             <StarIcon />
