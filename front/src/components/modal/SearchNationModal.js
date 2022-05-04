cd944ccacfd37d6f131eb1faaea7a9510713a2b7
bsy1141 <bsy1141@naver.com>
2022-05-04 22:36:11 +0900

    feat: 검색창 구현 완료

--- front/src/components/modal/SearchNationModal.js
+++ front/src/components/modal/SearchNationModal.js
@@ -1,26 +1,25 @@
-import React from "react";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useMemo } from "react";
 import { useNavigate } from "react-router-dom";
 import styles from "../../css/modal/SearchNationModal.module.css";
 import { countries } from "../../data/MapConstant";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import {
+  faMagnifyingGlass,
+  faCircleXmark,
+} from "@fortawesome/free-solid-svg-icons";
 
 const SearchNationModal = ({ setIsModalOpen }) => {
   const navigate = useNavigate();
   const [keyword, setKeyword] = useState("");
 
-  const searchedList = React.useMemo(() => {
+  const searchedList = useMemo(() => {
     if (keyword !== "") {
-      return countries.filter((c) => c.toLowerCase().includes(keyword));
+      return countries.filter((c) =>
+        c.toLowerCase().includes(keyword.toLowerCase()),
+      );
     }
     return [];
   }, [keyword]);
-
-  const updateChange = (e) => {
-    const target = e.target;
-    setKeyword(target.value);
-  };
 
   const handleClick = (nation) => {
     navigate(`/detail?country=${nation}`);
@@ -38,28 +37,38 @@ const SearchNationModal = ({ setIsModalOpen }) => {
     <div className={styles.container}>
       <section>
         <div className={styles.input}>
-          <span>
-            <FontAwesomeIcon icon={faMagnifyingGlass} />
+          <div className={styles.input_form}>
+            <span>
+              <FontAwesomeIcon icon={faMagnifyingGlass} />
+            </span>
+            <input
+              type="text"
+              value={keyword}
+              placeholder="국가를 입력해주세요..."
+              onChange={(e) => setKeyword(e.target.value)}
+            />
+          </div>
+          <span onClick={() => setKeyword("")} style={{ cursor: "pointer" }}>
+            <FontAwesomeIcon icon={faCircleXmark} />
           </span>
-          <input
-            type="text"
-            value={keyword}
-            placeholder="국가를 입력해주세요..."
-            onChange={updateChange}
-          />
         </div>
-        <div className={styles.result}>
-          {searchedList.length === 0 ? (
-            <div>검색 결과가 없습니다.</div>
-          ) : (
-            searchedList.map((nation) => (
+        {searchedList.length === 0 ? (
+          <div style={{ textAlign: "center" }}>검색 결과가 없습니다.</div>
+        ) : (
+          <div className={styles.result}>
+            {searchedList.map((nation) => (
               <div className={styles.result_item}>
                 <p onClick={() => handleClick(nation)}>{nation}</p>
               </div>
-            ))
-          )}
-        </div>
-        <button onClick={() => setIsModalOpen(false)}>close</button>
+            ))}
+          </div>
+        )}
+        <button
+          onClick={() => setIsModalOpen(false)}
+          className={styles.closeButton}
+        >
+          닫기
+        </button>
       </section>
     </div>
   );
