ea77e9b6f1dc35103ebd560a8e87aae2fe2e562d
bsy1141 <bsy1141@naver.com>
2022-05-04 17:22:39 +0900

    style: 국가 검색 모달 스타일링, chore: 모달 페이지 구성 잡기

--- front/src/components/map/MapMainPage.js
+++ front/src/components/map/MapMainPage.js
@@ -1,13 +1,23 @@
 import React, { useState } from "react";
 import ReactTooltip from "react-tooltip";
 import WorldMap from "./WorldMap";
+import styles from "../../css/map/MapMainPage.module.css";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const MapMainPage = () => {
   const [content, setContent] = useState("");
+  const [openModal, setOpenModal] = useState(false);
   return (
     <div>
       <WorldMap setTooltipContent={setContent} />
       <ReactTooltip>{content}</ReactTooltip>
+      <button
+        className={styles.button_search}
+        onClick={() => setOpenModal(true)}
+      >
+        <FontAwesomeIcon icon={faMagnifyingGlass} className={styles.icon} />
+      </button>
     </div>
   );
 };
---
+++ front/src/components/modal/SearchNationModal.js
@@ -0,0 +1,11 @@
+import styles from "../../css/modal/SearchNationModal.module.css";
+
+const SearchNationModal = () => {
+  return (
+    <div className={styles.container}>
+      <h3>searchModal</h3>
+    </div>
+  );
+};
+
+export default SearchNationModal;
